889da3435b15ff96ee8a2aed88502d4069ca865f
Sriram-1811 <sriramreddy.b2002@gmail.com>
2025-02-17 16:28:51 +0530

    new

---
+++ python miscellaneous/largest_smallest.py
@@ -0,0 +1,8 @@
+"""6. Write a program to find the largest and smallest numbers in a list."""
+
+def s_n_g(list):
+    print("the maximum value is :",max(list))
+    print("the minimum valure is:",min(list))
+
+list1=[1,2,3,4,5]
+s_n_g(list1)
---
+++ python miscellaneous/rm_duplicates.py
@@ -0,0 +1,6 @@
+"""7. Create a function that removes all duplicates from a list while maintaining order."""
+def rm_dup(lis):
+    return print(set(lis))
+
+list1=[11,22,3,3,44,5,5,7,7,777,6,5,4,3,3,4,4]
+rm_dup(list1)
